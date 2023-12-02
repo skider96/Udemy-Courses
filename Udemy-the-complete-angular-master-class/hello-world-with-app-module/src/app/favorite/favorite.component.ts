b71cf27f05f538d10a8b628733fdc0ee24e7d39e
skider96 <danailqkiq@gmail.com>
2023-12-02 18:09:07 +0200

    studying

--- Udemy-the-complete-angular-master-class/hello-world-with-app-module/src/app/favorite/favorite.component.ts
+++ Udemy-the-complete-angular-master-class/hello-world-with-app-module/src/app/favorite/favorite.component.ts
@@ -2,22 +2,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'favorite',
-  template: `
-    <span class="star" [class.active]="isFavorite" (click)="toggleStar()">&#9733;</span>
-  `,
-  styles: [`
-    .star {
-      font-size: 24px;
-      cursor: pointer;
-    }
-
-    .active {
-      color: gold;
-    }
-  `],
+  templateUrl: './favorite.component.html',
+  styleUrl: './favorite.component.css'
 })
 export class FavoriteComponent {
-  @Input() isFavorite: boolean = false;
+  @Input('is-favorite') isFavorite: boolean = false;
   @Output() starClicked = new EventEmitter<void>();
 
   toggleStar() {
---
+++ Udemy-the-complete-angular-master-class/hello-world-with-app-module/src/app/panel/panel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bootstrap-panel',
+  templateUrl: './panel.component.html',
+  styleUrl: './panel.component.css'
+})
+export class PanelComponent {
+title: any;
+constructor () {
+  
+}
+}
