8945d8833ae8854ae2e6ce629677de96c4a916f5
skider96 <danailqkiq@gmail.com>
2023-12-28 19:23:20 +0200

    studying

--- Udemy-the-complete-angular-master-class/hello-world-with-app-module/src/app/posts/posts.component.ts
+++ Udemy-the-complete-angular-master-class/hello-world-with-app-module/src/app/posts/posts.component.ts
@@ -1,30 +1,46 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpResponse  } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'posts',
   templateUrl: './posts.component.html',
-  styleUrl: './posts.component.css'
+  styleUrl: './posts.component.css',
 })
 export class PostsComponent {
   posts: any[];
   private url = 'https://jsonplaceholder.typicode.com/posts';
   constructor(private httpClient: HttpClient) {
-    this.httpClient.get<any>(this.url)
-      .subscribe(response => {
-        console.log(response);
-        this.posts = response;
-      })
+    this.httpClient.get<any>(this.url).subscribe((response) => {
+      console.log(response);
+      this.posts = response;
+    });
   }
 
   createPost(input: HTMLInputElement) {
-let post: any = { title: input.value} ;
-this.httpClient.post(this.url, JSON.stringify(post))
-.subscribe((response: any) => {
-  post.id = response.id;
-  this.posts.splice(0,0, post)
-  console.log(response);
-});
-input.value = '';
-}
+    let post: any = { title: input.value };
+    input.value = '';
+    this.httpClient
+      .post(this.url, JSON.stringify(post))
+      .subscribe((response: any) => {
+        post.id = response.id;
+        this.posts.splice(0, 0, post);
+        console.log(response);
+      });
+  }
+  updatePost(post: any) {
+    this.httpClient
+      .patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .subscribe((response: any) => {
+        console.log(response);
+      });
+  }
+
+  deletePost(post: any) {
+    this.httpClient
+      .delete(this.url + '/' + post.id)
+      .subscribe((response: any) => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      });
+  }
 }
