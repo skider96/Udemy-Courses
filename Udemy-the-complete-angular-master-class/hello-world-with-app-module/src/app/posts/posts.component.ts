bb90790807c999e4e279882fd563aaabe68b648f
skider96 <danailqkiq@gmail.com>
2024-01-02 20:29:38 +0200

    studying

--- Udemy-the-complete-angular-master-class/hello-world-with-app-module/src/app/posts/posts.component.ts
+++ Udemy-the-complete-angular-master-class/hello-world-with-app-module/src/app/posts/posts.component.ts
@@ -6,42 +6,55 @@ import { PostService } from '../services/post.service';
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css',
 })
-export class PostsComponent implements OnInit  {
+export class PostsComponent implements OnInit {
   posts: any[];
-  constructor(private service: PostService) {
-  
-  }
+  constructor(private service: PostService) {}
 
   createPost(input: HTMLInputElement) {
     let post: any = { title: input.value };
     input.value = '';
-    this.service.createPosts(post)
-      .subscribe((response: any) => {
+    this.service.createPosts(post).subscribe(
+      (response: any) => {
         post.id = response.id;
         this.posts.splice(0, 0, post);
         console.log(response);
-      });
+      },
+      (error) => alert('Cannot create post')
+    );
   }
   updatePost(post: any) {
-   this.service.updatePosts(post)
-      .subscribe((response: any) => {
+    this.service.updatePosts(post).subscribe(
+      (response: any) => {
         console.log(response);
-      });
+      },
+      (error) => alert('Error updating')
+    );
   }
 
   deletePost(post: any) {
- this.service.deletePosts(post.id)
-      .subscribe((response: any) => {
+    this.service.deletePosts(post.id)
+    .subscribe(
+      (response: any) => {
         let index = this.posts.indexOf(post);
         this.posts.splice(index, 1);
-      });
+      },
+      (error: Response) => {
+        if (error.status === 404) {
+          alert('Error deleting post');}
+       else { 
+        alert('common error');
+      }
+  });
   }
   ngOnInit() {
-this.service.getPosts()
-    .subscribe((response) => {
-      console.log(response);
-      this.posts = response;
-    });
+    this.service.getPosts().subscribe(
+      (response) => {
+        console.log(response);
+        this.posts = response;
+      },
+      (error) => {
+        alert('Unexpected error');
+      }
+    );
   }
 }
-
