6b69e1d88e8ed22cac49cb5d03c7d4963749a8a7
skider96 <danailqkiq@gmail.com>
2024-01-19 21:34:27 +0200

    studying 91th lesson

--- Udemy-the-complete-angular-master-class/shop/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ Udemy-the-complete-angular-master-class/shop/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,13 +9,21 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
 recipes: Recipe[] = [
   new Recipe('A test Recipe', 'This is simply test', 
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70'
-)];
+  'https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70'),
+  new Recipe('Another test Recipe', 'This is simply test', 
+  'https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70'),
+]
+@Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() {
 
 }
 
 ngOnInit() {
 
+}
+
+
+onRecipeSelected(recipe: Recipe) {
+this.recipeWasSelected.emit(recipe);
 }
 }
