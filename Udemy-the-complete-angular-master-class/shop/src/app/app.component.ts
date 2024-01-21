27786f16d7568b65d04a80ea1b59422c70b648c1
skider96 <danailqkiq@gmail.com>
2024-01-21 14:12:41 +0200

    studying 93th lesson (strict)

--- Udemy-the-complete-angular-master-class/shop/src/app/app.component.ts
+++ Udemy-the-complete-angular-master-class/shop/src/app/app.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-loadedFeature = 'recipe';
+  loadedFeature = 'recipe';
 
-onNavigate(feature: string) {
-this.loadedFeature = feature;
-}
-  title = 'shop';
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
--- Udemy-the-complete-angular-master-class/shop/src/app/app.module.ts
+++ Udemy-the-complete-angular-master-class/shop/src/app/app.module.ts
@@ -1,33 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipesComponent } from './recipe-book/recipes.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingListComponent,
+    HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
-    RecipeItemComponent,
     RecipeDetailComponent,
-    ShoppingEditComponent,
-    HeaderComponent
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
