cc2f90a3c6aefc5eb65abccc67629d609b7d31cf
skider96 <danailqkiq@gmail.com>
2024-01-30 12:41:18 +0200

    studying 117th lesson

--- Udemy-the-complete-angular-master-class/services-start/src/app/app.module.ts
+++ Udemy-the-complete-angular-master-class/services-start/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountsService } from './new-account/accounts.service';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { NewAccountComponent } from './new-account/new-account.component';
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Udemy-the-complete-angular-master-class/services-start/src/app/new-account/new-account.component.ts
+++ Udemy-the-complete-angular-master-class/services-start/src/app/new-account/new-account.component.ts
@@ -7,17 +7,18 @@ import { AccountsService } from './accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+ // providers: [LoggingService]
 })
 export class NewAccountComponent {
 
   constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
-
+this.accountsService.statusUpdated.subscribe(
+  (status: string) => alert('New status: ' + status));
   }
   onCreateAccount(accountName: string, accountStatus: string) {
 
 this.accountsService.addAccount(accountName, accountStatus)
-    this.loggingService.logStatusChange(accountStatus);
+   // this.loggingService.logStatusChange(accountStatus);
   }
 
 }
